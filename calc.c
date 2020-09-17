e899e06d235bf62fe5f0b749ac4dfd5775ebebc3
SPxRTxN <kotov0104@mail.ru>
2020-09-17 21:48:55 +0300

    short code

--- calc.c
+++ calc.c
@@ -1,43 +1,25 @@
-#include <stdio.h>                                     
-
-int main(){                                            
-
-    while (1) {                                         
-
-		char action;                                    
-    	long long n1, n2, sum, diff, mult, split;      
-
-		printf("%s", "input +,-,/,* :");               
-		scanf("%c", &action);                           
-
-		printf("%s", "input number 1:");                
-		scanf("%d", &n1);                               
-		printf("%s", "input number 2:");                
-		scanf("%d", &n2);                               
-
-
+#include <stdio.h>  
+int main(){   
+	char action = '+';                                    
+    long long n1, n2;
+    do{ 
+		printf("input num1 ""+-*/"" num2: ");               
+		scanf("%d%s%d", &n1, &action, &n2);
 		switch (action) {                                     
-            case '*':                                       
-			mult = n1 * n2;                             
-			printf("%s %d\n", "Mult:", mult);           
-			break;                                     
-		case '+':                                       
-			sum = n1 + n2;                              
-			printf("%s %d\n", "Sum:", sum);             
-			break;                                      
+            case '*':                            
+			printf("Mult: %d\n", n1 * n2);           
+			break;                                
 		case '/':                                       
-			split = n1 / n2;                            
-			printf("%s %d\n", "Split:", split);         
+			printf("Split: %d\n", n1 / n2);         
 			break;                                      
 		case '-':                                                                      
-			diff = n1 - n2;                             
-			printf("%s %d\n", "Diff:", diff);          
-			break;                                      
-		}                                               
-
-        n1 = 0;                                          
-        n2 = 0;
-	}                                                   
+			printf("Diff: %d\n", n1 - n2);          
+			break; 
+		case '+':                                       
+			printf("Sum: %d\n", n1 + n2);             
+			break;                                           
+		}   
+	}  while(action != '0');
 	return 0;                                                           
-}                                                       
+}                                                 
 
