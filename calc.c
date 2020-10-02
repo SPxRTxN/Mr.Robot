4c1962e073a76567bcb5b8212c1c20c87d30634b
SPxRTxN <kotov0104@mail.ru>
2020-10-02 19:35:45 +0300

    add new futures

--- calc.c
+++ calc.c
@@ -4,22 +4,25 @@ int main(){
     long long n1, n2;
     do{ 
 		printf("input num1 ""+-*/"" num2: ");               
-		scanf("%d%s%d", &n1, &action, &n2);
+		scanf("%d%s%d",&n1,&action,&n2);
 		switch (action) {                                     
             case '*':                            
 			printf("Mult: %d\n", n1 * n2);           
 			break;                                
-		case '/':                                       
+		case '/':       
+			if (n2 != 0){                               
 			printf("Split: %d\n", n1 / n2);         
-			break;                                      
+			break;
+			}else
+				continue;                            
 		case '-':                                                                      
 			printf("Diff: %d\n", n1 - n2);          
 			break; 
 		case '+':                                       
 			printf("Sum: %d\n", n1 + n2);             
-			break;                                           
+			break;                                      
 		}   
-	}  while(action != '0');
+		}  while(action != '0');
 	return 0;                                                           
 }                                                 
 
